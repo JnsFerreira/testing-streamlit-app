e1e9a994f6dfd00e9e1a2d190bc6c592e557d6f9
JnsFerreira <jonas.abreuferreira@gmail.com>
2020-06-08 22:23:20 -0300

    change login code

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ session_state = get(password='')
 
 if session_state.password != 'pwd123':
     pwd_placeholder = st.sidebar.empty()
-    pwd = pwd_placeholder.text_input("Password:", value="")
+    pwd = pwd_placeholder.text_input("Password:", value="", type='password')
     session_state.password = pwd
     if session_state.password == 'pwd123':
         pwd_placeholder.empty()
